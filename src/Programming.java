71499349e7124077d56a40b5dba4aa4849c625f9
AlexWroclawJava <aleksander.szlachcic@gmail.com>
2017-11-24 02:30:08 +0100

    Stworzenie pierwszego commit'a

---
+++ src/Programming.java
@@ -0,0 +1,12 @@
+public class Programming {
+
+    public static void main(String[] args) {
+
+        System.out.println("Uczę");
+        System.out.println("się");
+        System.out.println("programowania");
+
+    }
+
+}
+
